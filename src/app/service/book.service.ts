77387f7212b40feb5d5b391d9e7f88fec1cc91e0
PicToWeb <picronantoine34@gmail.com>
2024-08-01 23:30:29 +0200

    Ajout navbar, ajout suppression depuis edition, pop up confirmation et detail du livre

--- src/app/service/book.service.ts
+++ src/app/service/book.service.ts
@@ -19,12 +19,26 @@ export class BookService {
   /** The subject that emits the books */
   private booksSubject = new BehaviorSubject<Book[]>([]);
 
+  /**
+   * Constructor of the service to inject the HTTP client
+   * @param http The HTTP client to make requests
+   */
   constructor(private http: HttpClient) {}
 
+  /**
+   * Get all books from the server
+   * @param id The id of the todo to get
+   * @returns An observable that emits the books
+   */
   getBookById(id:string):Observable<Book>{
     return this.http.get<Book>(`${this.apiURL}/${id}`);
   }
 
+  /**
+   * Get all books from the server
+   * @param book The book to add
+   * @returns An observable that emits the books
+   */
   updateData(book: Book): Observable<void> {
     return this.http.put<void>(`${this.apiURL}/${book.id}`, book).pipe(
       map(() => {
@@ -48,5 +62,18 @@ export class BookService {
         },
       });
     });
+  }
+
+  /**
+   * delete a book from the server
+   * @param id The id of the book to delete
+   * @returns An observable that emits the books
+   */
+  removeBook(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiURL}/${id}`).pipe(
+      map(() => {
+        this.booksSubject.next([...this.booksSubject.value.filter(b => b.id !== id)]);
+      })
+    );
   }
 }
