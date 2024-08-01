b2a1547042b74f34f7dac85a365e2c02bc6e5a7d
ThomasdevLC <thomas.lecam@gmail.com>
2024-08-01 16:33:58 +0200

    add book + form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
-import {BookListComponent} from "./component/book-list/book-list.component";
-import {EditBookComponent} from "./component/edit-book/edit-book.component";
+import { BookListComponent } from './component/book-list/book-list.component';
+import { EditBookComponent } from './component/edit-book/edit-book.component';
+import { AddBookComponent } from './component/add-book/add-book.component';
 
 export const routes: Routes = [
-  {path:'',component:BookListComponent},
-  // {path:'edit',component:EditBookComponent}
-
+  { path: 'books', component: BookListComponent },
+  { path: 'add', component: AddBookComponent },
+  { path: 'edit/:id', component: EditBookComponent },
 ];
--- src/app/component/add-book/add-book.component.ts
+++ src/app/component/add-book/add-book.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
-
+import { BookService, Book } from '../../service/book.service';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { computeMsgId } from '@angular/compiler';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-add-book',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './add-book.component.html',
-  styleUrl: './add-book.component.css'
+  styleUrls: ['./add-book.component.css'],
 })
 export class AddBookComponent {
+  newBook: Book = {
+    title: '',
+    author: '',
+    description: '',
+    status: 'available',
+  };
+  errorMessage: string | null = null;
 
+  constructor(private bookService: BookService, private router: Router) {}
+
+  onSubmit() {
+    if (
+      !this.newBook.title ||
+      !this.newBook.author ||
+      !this.newBook.description ||
+      !this.newBook.status
+    ) {
+      this.errorMessage = 'Vous devez renseigner tous les champs.';
+      return;
+    }
+
+    this.errorMessage = null;
+
+    this.bookService.addBook(this.newBook).subscribe({
+      next: (book) => {
+        console.log('Book added:', book);
+        this.newBook = {
+          title: '',
+          author: '',
+          description: '',
+          status: 'available',
+        };
+        this.router.navigate(['/books']);
+      },
+      error: (err) => {
+        console.error('Error adding book:', err);
+        this.errorMessage = 'Error adding book';
+      },
+    });
+  }
 }
