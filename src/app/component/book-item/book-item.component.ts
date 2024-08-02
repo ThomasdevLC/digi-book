aadd4b93dafbd2683250812cf9d03d7a2febc9a7
PicToWeb <picronantoine34@gmail.com>
2024-08-02 12:45:39 +0200

    adjust style for list

--- src/app/component/book-item/book-item.component.ts
+++ src/app/component/book-item/book-item.component.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { FilterByStatusPipe } from '../../pipe/filter-by-status.pipe';
 import { StatusPipe } from '../../pipe/status.pipe';
 import { FilterByNamePipe } from '../../pipe/filter-by-name.pipe';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-book-item',
   standalone: true,
-  imports: [CommonModule, FilterByStatusPipe, StatusPipe, FilterByNamePipe],
+  imports: [CommonModule, FilterByStatusPipe, StatusPipe, FilterByNamePipe, RouterLink],
   templateUrl: './book-item.component.html',
   styleUrls: ['./book-item.component.css'],
 })
